3da58cfa94542ce0e9280bd03705481e4306a1c8
Shark176 <91861912+Shark176@users.noreply.github.com>
2022-10-15 23:49:48 +0700

    Update UIT_CAR_RACING.py

--- UIT_CAR_RACING.py
+++ UIT_CAR_RACING.py
@@ -5,7 +5,6 @@ from controller import Camera
 from controller import LED
 from controller import Supervisor
 import math
-import time
 robot = Robot()
 
 # get the time step of the current world
@@ -35,25 +34,6 @@ for i in range(NB_GROUND_SENS):
     gs.append(robot.getDevice(gsNames[i]))
     gs[i].enable(timestep)
 
-# LEDs
-NB_LEDS = 5
-leds = []
-led_Names = [
-    'led0', 'led1', 'led2', 'led3', 'led4'
-]
-for i in range(NB_LEDS):
-    leds.append(robot.getDevice(led_Names[i]))
-
-### Private Functions ###
-# Function to control LEDs
-def LED_Alert():
-    if (robot.getTime() - initTime)*1000 % 3000 >= 2000:
-        #leds[1].set(not(leds[1].get()))
-        leds[1].set(1)
-        #for i in range(NB_LEDS):
-            #leds[i].set(not(leds[i].get()))
-    return
-
 # Waiting for completing initialization
 ### thí sinh không được bỏ phần này
 initTime = robot.getTime()
@@ -61,20 +41,21 @@ while robot.step(timestep) != -1:
     if (robot.getTime() - initTime) * 1000.0 > 200:
         break
 
-
 ### Phần code cần chỉnh sửa cho phù hợp ##
 # Định nghĩa các tín hiệu của xe
-NOP = -4
+NOP = -3
 MID = 0
 LEFT = 1
 RIGHT = -1
-RIGHT_VUONG = -2
-LEFT_VUONG = 2
-FULL_SIGNAL  = 3
-BLANK_SIGNAL = -3
+LEFT_VUONG = 4
+RIGHT_VUONG = -4
+LEFT_MID = 5
+RIGHT_MID = -5
+FULL_SIGNAL  = 2
+BLANK_SIGNAL = -2
 
 # MAX_SPEED <= 1000
-MAX_SPEED = 30
+MAX_SPEED = 40
 threshold = [330, 330, 330, 330, 330, 330, 330, 330]
 preFilted = 0b00000000
 
@@ -90,126 +71,195 @@ def ReadSensors():
         gsValues.append(gs[i].getValue())
         if gsValues[i] > threshold[i]:
             filted |= (0x01 << (NB_GROUND_SENS - i - 1))
-    #print(*gsValues, sep = '\t')
+    #print(gs[7].getValue(), sep = '\t')
     return filted
 
 # Phần code điều khiển xe
-
-#xác định độ lệch của xe từ việc đọc giá trị cảm biến
 def DeterminePosition(filted):
-    if (filted == 0b11100111 or filted == 0b11101111 or filted == 0b11110111):
+    if filted == 0b11100111 or filted == 0b11000011 or filted == 0b10000001:
         return MID
-    elif (filted == 0b10011111 or filted == 0b11001111 or filted == 0b11011111 or filted == 0b10111111 or filted == 0b11101111
-    or filted == 0b01111111 or filted == 0b00111111 ):
-        return LEFT
-    elif (filted == 0b11111001 or filted == 0b11110011 or filted == 0b11111101 or filted == 0b11111011 or filted == 0b11110111
-    or filted == 0b11111110 or filted == 0b11111100 ):
-        return RIGHT
-    elif filted == 0b11111111:
-        return BLANK_SIGNAL
-    elif filted == 0b00000000:
-        return FULL_SIGNAL
-    elif (filted == 0b10000111 or filted == 0b00000111 or filted == 0b00001111 or filted == 0b00011111 or filted == 0b11000111):
+    elif (str_filted[0] == '0' and str_filted[5:8] == '111') or filted == 0b10000111: #or filted == 0b10000111 or str_filted[:4] == '0':
         return LEFT_VUONG
-    elif (filted == 0b11100001 or filted == 0b11100000 or filted == 0b11110000 or filted == 0b11111000 or filted == 0b11100011):
+    elif (str_filted[4:8] == '1111' and str_filted[:4] != '1111'):
+        return LEFT
+    elif ((str_filted[7] == '0') and (str_filted[:3] == '111')) or filted == 0b11100001:# or filted == 0b11100001 or str_filted[3:8] == '0':
         return RIGHT_VUONG
-    return MID
-       
+    elif ((str_filted[:4] == '1111') and (str_filted[4:8] != '1111')):
+        return RIGHT
+    elif filted == 0b11111111 or filted == 0b11101111 or filted == 0b11110111:
+        return FULL_SIGNAL
+    elif filted == 0b00000000:
+        return BLANK_SIGNAL
+    return NOP
+
 #các hàm điều khiển xe di chuyển
-def GoStraight():
-    lm.setVelocity(1.0 * MAX_SPEED)
-    rm.setVelocity(1.0 * MAX_SPEED)
-    return
-        
-def TurnLeft():
-    #if pos == :
+def GoStraight(filted):
+    lm.setVelocity(1 * MAX_SPEED)
+    rm.setVelocity(1 * MAX_SPEED)
+
+def TurnLeft(filted):
+    if filted == 0b11101111:
+        lm.setVelocity(0.866 * MAX_SPEED)
+        rm.setVelocity(1.0 * MAX_SPEED)
+        print('left lil')
+    elif filted == 0b11001111:
+        lm.setVelocity(0.707 * MAX_SPEED)
+        rm.setVelocity(1.0 * MAX_SPEED)
+        print('left med')
+    elif filted == 0b10011111:
+        lm.setVelocity(0.5 * MAX_SPEED)
+        rm.setVelocity(1.0 * MAX_SPEED)
+        print('left big')
     lm.setVelocity(0.5 * MAX_SPEED)
     rm.setVelocity(1.0 * MAX_SPEED)
-    return  
-def TurnRight():
-    #if pos == :
+
+def TurnRight(filted):
+    if filted == 0b11110111:
+        lm.setVelocity(1.0 * MAX_SPEED)
+        rm.setVelocity(0.866 * MAX_SPEED)
+        print('right lil')
+    elif filted == 0b11110011:
+        lm.setVelocity(1.0 * MAX_SPEED)
+        rm.setVelocity(0.707 * MAX_SPEED)
+        print('right med')
+    elif filted == 0b11111001:
+        lm.setVelocity(1.0 * MAX_SPEED)
+        rm.setVelocity(0.5 * MAX_SPEED)
+        print('right big')
     lm.setVelocity(1.0 * MAX_SPEED)
     rm.setVelocity(0.5 * MAX_SPEED)
-    return
+    return 1.0, 0.5
 
-def TurnLeftVuong():
-    for i in range(52):
-        a = robot.step(timestep)
-        lm.setVelocity(0.3 * MAX_SPEED)
-        rm.setVelocity(1.3 * MAX_SPEED)
-        
-    return
 def TurnRightVuong():
-    for i in range(52):
-        a = robot.step(timestep)
-        lm.setVelocity(1.3 * MAX_SPEED)
-        rm.setVelocity(0.3 * MAX_SPEED)
-        
-    return
-def NgaTuLeft():
-    print('T_L')
-    for i in range(160):
-        
-        a = robot.step(timestep)
-        lm.setVelocity(0.67 * MAX_SPEED)
-        rm.setVelocity(1 * MAX_SPEED)
-    return
-def NgaTuRight():
-    print('T_R')
-    for i in range(160):
-     
-        a = robot.step(timestep)
-        lm.setVelocity(1 * MAX_SPEED)
-        rm.setVelocity(0.67 * MAX_SPEED)
+    lm.setVelocity(1 * MAX_SPEED)
+    rm.setVelocity(0.15 * MAX_SPEED)
+    while robot.step(timestep) != -1:
+        filted = ReadSensors()
+        pos = DeterminePosition(filted)
+        if pos == MID:
+            return
+def TurnLeftVuong():
+     lm.setVelocity(0.15 * MAX_SPEED)
+     rm.setVelocity(1 * MAX_SPEED)
+     while robot.step(timestep) != -1:
+        filted = ReadSensors()
+        pos = DeterminePosition(filted)
+        if pos == MID:
+            return
+def TurnLeftMid():
+    lm.setVelocity(0.5 * MAX_SPEED)
+    rm.setVelocity(1 * MAX_SPEED)
+    while robot.step(timestep) != -1:
+        filted = ReadSensors()
+        pos = DeterminePosition(filted)
+        if pos == MID:
+            return
 
-    return
+def TurnRightMid():
+    lm.setVelocity(1 * MAX_SPEED)
+    rm.setVelocity(0.5 * MAX_SPEED)
+    while robot.step(timestep) != -1:
+        filted = ReadSensors()
+        pos = DeterminePosition(filted)
+        if pos == MID:
+            return
+def NgaTuLeft():
+    for i in range(51):
+        robot.step(timestep)
+        lm.setVelocity(0.15 * MAX_SPEED)
+        rm.setVelocity(1 * MAX_SPEED)
+def NgaTuRight():
+    for i in range(51):
+        robot.step(timestep)
+        lm.setVelocity(1 * MAX_SPEED)
+        rm.setVelocity(0.15 * MAX_SPEED)
+def VongXuyen():
+    for i in range(48):
+        robot.step(timestep)
+        lm.setVelocity(1 * MAX_SPEED)
+        rm.setVelocity(0.15 * MAX_SPEED)
+
 lastPos = 0  
+NgaTuLeft_Signal = False
+NgaTuRight_Signal = False
+VongXuyen_Signal = False
+BLANK_SIGNAL_COUNTER = 0
 # Main loop:
 # Chương trình sẽ được lặp lại vô tận 
 while robot.step(timestep) != -1:
-
     filted = ReadSensors()
-    
     #pos: position - vị trí của xe
+    str_filted = (str(bin(filted))[2:]).zfill(8)
     pos = DeterminePosition(filted)
     # In ra màn hình giá trị của filted ở dạng nhị phân
     print('Position: ' + str(format(filted, '08b')), sep = '\t')
-    print(pos )
-    print('lastpos' + str(lastPos))
-    #Gọi các hàm điều khiển
-    if (pos == MID and lastPos == MID):
-        GoStraight()
-    elif (pos == LEFT and lastPos == MID) or (pos == LEFT and lastPos == LEFT) :
-        TurnLeft()
-    elif (pos == RIGHT and lastPos == MID) or (pos == RIGHT and lastPos == RIGHT):
-        TurnRight() 
-        
-        
-    if ((pos == BLANK_SIGNAL and lastPos ==LEFT_VUONG) or (pos == LEFT and lastPos ==LEFT_VUONG)) :
-       print('LV')
-       TurnLeftVuong()     
-    elif ((pos == BLANK_SIGNAL and lastPos ==RIGHT_VUONG) or (pos == RIGHT and lastPos ==RIGHT_VUONG) ):
-      print('RV')
-      TurnRightVuong() 
-      
-      
-    elif ((pos == MID and lastPos == LEFT_VUONG)) :
-       print("NTL---------------------")
-       NgaTuLeft();
-    elif ((pos == MID and lastPos ==RIGHT_VUONG)) :
-       print("NTR-------------------------")
-       NgaTuRight();
-    
-     
-    elif pos == BLANK_SIGNAL:
-        GoStraight()
-    
-  
-    
-      
+    if pos == MID:
+        GoStraight(filted)
+        print('straight')
+    elif filted == 0b00111100 or filted == 0b00011000 and VongXuyen_Signal:
+        VongXuyen()
+        print('vong xoay')
+    elif pos == RIGHT_VUONG and VongXuyen_Signal:
+        for i in range(40):
+            robot.step(timestep)
+            lm.setVelocity(1 * MAX_SPEED)
+            rm.setVelocity(0 * MAX_SPEED)
+        VongXuyen_Signal = False 
+        BLANK_SIGNAL_COUNTER = 0
+    elif preFilted == 0b00000000 and filted != 0b00000000:
+        if NgaTuRight_Signal:
+            NgaTuRight()
+            print('RE NGA TU R')
+            NgaTuRight_Signal = False 
+            VongXuyen_Signal = False
+            BLANK_SIGNAL_COUNTER = 0
+            print('right 4')
+        elif NgaTuLeft_Signal:
+            NgaTuLeft()
+            print('RE NGA TU L')
+            NgaTuLeft_Signal = False
+            VongXuyen_Signal = False 
+            BLANK_SIGNAL_COUNTER = 0
+            print('left 4')
+        else:
+            VongXuyen_Signal = True
+            for i in range(5):
+                robot.step(timestep)
+                lm.setVelocity(1 * MAX_SPEED)
+                rm.setVelocity(1 * MAX_SPEED)  
+    elif lastPos == LEFT_VUONG and filted == 0b11111111:# or filted == 0b01111111 or filted == 0b11111110):
+        print('left vuong')
+        TurnLeftVuong()
+    elif lastPos == RIGHT_VUONG and filted == 0b11111111:# or filted == 0b01111111 or filted == 0b11111110):
+        print('right vuong')
+        TurnRightVuong() 
+    elif lastPos == MID and filted == 0b11110011:
+        print('right mid')
+        TurnRightMid()
+    elif lastPos == MID and filted == 0b11001111:
+        print('left mid')
+        TurnLeftMid() 
+    elif pos == RIGHT:
+        TurnRight(filted)
+    elif pos == LEFT:
+        TurnLeft(filted)  
+    elif BLANK_SIGNAL_COUNTER >= 30:
+        lm.setVelocity(0 * MAX_SPEED)
+        rm.setVelocity(0 * MAX_SPEED)     
+    else:  
+        lm.setVelocity(1 * MAX_SPEED)
+        rm.setVelocity(1 * MAX_SPEED) 
+    if (lastPos == LEFT_VUONG or preFilted == 0b00000111 or preFilted == 0b10000111) and pos != FULL_SIGNAL and pos != LEFT_VUONG and (pos == MID or filted == 0b11110111 or filted == 0b11101111):
+        NgaTuLeft_Signal = True
+    elif (lastPos == RIGHT_VUONG or preFilted == 0b11100000 or preFilted == 0b11100001) and pos != FULL_SIGNAL and pos != RIGHT_VUONG and (pos == MID or filted == 0b11101111 or filted == 0b11110111):
+        NgaTuRight_Signal = True
+    if pos == BLANK_SIGNAL:
+        BLANK_SIGNAL_COUNTER += 1
+    print('right NT', NgaTuRight_Signal)
+    print('left NT', NgaTuLeft_Signal)
+    print('Vong xuyen: ', VongXuyen_Signal)
+    print('Signal BL: ', str(BLANK_SIGNAL_COUNTER))
     preFilted = filted
     lastPos = pos
-    
+    print('-----------------------------')
     pass
-
-# Enter here exit cleanup code.D
